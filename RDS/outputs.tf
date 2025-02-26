e0d33fe67423cbf8dfefbd89d3c53562a9561107
Junho-06 <k34920032@gmail.com>
2025-02-26 11:40:19 +0900

    delete annotation RDS output

--- RDS/outputs.tf
+++ RDS/outputs.tf
@@ -1,16 +1,3 @@
-# output "aurora_mysql_endpoint" {
-#   value = var.create_aurora_mysql_cluster == true ? aws_rds_cluster.aurora-mysql-cluster[0].endpoint : null
-# }
-# output "aurora_mysql_reader_endpoint" {
-#   value = var.create_aurora_mysql_cluster == true ? aws_rds_cluster.aurora-mysql-cluster[0].endpoint : null
-# }
-# output "aurora_postgres_endpoint" {
-#   value = var.create_aurora_postgres_cluster == true ? aws_rds_cluster.aurora-postgres-cluster[0].endpoint : null
-# }
-# output "aurora_postgres_reader_endpoint" {
-#   value = var.create_aurora_postgres_cluster == true ? aws_rds_cluster.aurora-postgres-cluster[0].endpoint : null
-# }
-
 output "rds" {
   value = <<EOT
     aurora_mysql_endpoint             = ${jsonencode(var.db_engine.create_aurora_mysql_cluster == true ? aws_rds_cluster.aurora-mysql-cluster[0].endpoint : null)}
