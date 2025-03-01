bcb31005fb80f7d7155ecf4b2722da3e216fd387
Junho-06 <k34920032@gmail.com>
2025-03-01 09:13:33 +0900

    update DynamoDB

--- DynamoDB/main.tf
+++ DynamoDB/main.tf
@@ -1,12 +1,16 @@
+# DynamoDB
+# ========================================================
 resource "aws_dynamodb_table" "dynamodb-table" {
-  name         = var.dynamodb.table_name
+  name = var.dynamodb.table_name
+
+  table_class  = var.dynamodb.table_class
   billing_mode = var.dynamodb.billing_mode
 
   hash_key  = var.dynamodb.partition_key
   range_key = try(var.dynamodb.sort_key, null)
 
-  read_capacity  = var.dynamodb.billing_mode == "PROVISIONED" ? try(var.dynamodb.read_capacity, null) : null
-  write_capacity = var.dynamodb.billing_mode == "PROVISIONED" ? try(var.dynamodb.write_capacity, null) : null
+  read_capacity  = var.dynamodb.billing_mode == "PROVISIONED" ? var.dynamodb.capacity_autoscaling_enable == false ? var.dynamodb.disable_auto_scaling.read_capacity : 1 : null
+  write_capacity = var.dynamodb.billing_mode == "PROVISIONED" ? var.dynamodb.capacity_autoscaling_enable == false ? var.dynamodb.disable_auto_scaling.write_capacity : 1 : null
 
   dynamic "attribute" {
     for_each = var.dynamodb.keys
@@ -18,7 +22,7 @@ resource "aws_dynamodb_table" "dynamodb-table" {
   }
 
   point_in_time_recovery {
-    enabled = true
+    enabled = var.dynamodb.PITR_enable
   }
 
   server_side_encryption {
@@ -30,6 +34,90 @@ resource "aws_dynamodb_table" "dynamodb-table" {
     "Name" = var.dynamodb.table_name
   }
 }
+
+
+# Capacity Auto Scaling
+# ========================================================
+resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
+  count = var.dynamodb.billing_mode == "PROVISIONED" && var.dynamodb.capacity_autoscaling_enable == true ? 1 : 0
+
+  max_capacity       = var.dynamodb.enable_auto_scaling.read_capacity_max
+  min_capacity       = var.dynamodb.enable_auto_scaling.read_capacity_min
+  resource_id        = "table/${var.dynamodb.table_name}"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+
+  depends_on = [aws_dynamodb_table.dynamodb-table]
+}
+
+resource "aws_appautoscaling_policy" "dynamodb_table_read_policy" {
+  count = var.dynamodb.billing_mode == "PROVISIONED" && var.dynamodb.capacity_autoscaling_enable == true ? 1 : 0
+
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_read_target[0].resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.dynamodb_table_read_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.dynamodb_table_read_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.dynamodb_table_read_target[0].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = var.dynamodb.enable_auto_scaling.read_capacity_utilization_percent
+  }
+
+  depends_on = [aws_dynamodb_table.dynamodb-table]
+}
+
+resource "aws_appautoscaling_target" "dynamodb_table_write_target" {
+  count = var.dynamodb.billing_mode == "PROVISIONED" && var.dynamodb.capacity_autoscaling_enable == true ? 1 : 0
+
+  max_capacity       = var.dynamodb.enable_auto_scaling.write_capacity_max
+  min_capacity       = var.dynamodb.enable_auto_scaling.write_capacity_min
+  resource_id        = "table/${var.dynamodb.table_name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+
+  depends_on = [aws_dynamodb_table.dynamodb-table]
+}
+
+resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
+  count = var.dynamodb.billing_mode == "PROVISIONED" && var.dynamodb.capacity_autoscaling_enable == true ? 1 : 0
+
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_write_target[0].resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.dynamodb_table_write_target[0].resource_id
+  scalable_dimension = aws_appautoscaling_target.dynamodb_table_write_target[0].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.dynamodb_table_write_target[0].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = var.dynamodb.enable_auto_scaling.write_capacity_utilization_percent
+  }
+
+  depends_on = [aws_dynamodb_table.dynamodb-table]
+}
+
+
+# Create DynamoDB Backup
+# ========================================================
+resource "null_resource" "trigger_backup" {
+  provisioner "local-exec" {
+    command = <<EOT
+      aws dynamodb create-backup --table-name ${var.dynamodb.table_name} --backup-name ${var.dynamodb.table_name}
+    EOT
+    when    = create
+  }
+  depends_on = [aws_dynamodb_table.dynamodb-table]
+}
+
+
+# CMK
+# ========================================================
 data "aws_caller_identity" "current" {}
 resource "aws_kms_key" "dynamodb-cmk" {
   description             = "DyanmoDB CMK"
@@ -54,12 +142,4 @@ resource "aws_kms_key" "dynamodb-cmk" {
 resource "aws_kms_alias" "dynamodb-cmk-alias" {
   name          = "alias/dynamodb-cmk"
   target_key_id = aws_kms_key.dynamodb-cmk.id
-}
-resource "null_resource" "trigger_backup" {
-  provisioner "local-exec" {
-    command = <<EOT
-      aws dynamodb create-backup --table-name ${var.dynamodb.table_name} --backup-name ${var.dynamodb.table_name}
-    EOT
-  }
-  depends_on = [aws_dynamodb_table.dynamodb-table]
 }
--- DynamoDB/variables.tf
+++ DynamoDB/variables.tf
@@ -2,12 +2,28 @@ variable "dynamodb" {
   type        = any
   description = "DynamoDB Variables"
   default = {
-    table_name   = "skills-dynamodb"
-    billing_mode = "PAY_PER_REQUEST" # PROVISIONED
+    table_name = "skills-dynamodb"
 
-    # if you want capacity auto scaling? -> create on console
-    read_capacity  = 10 # only use billing_mode is PROVISIONED
-    write_capacity = 10 # only use billing_mode is PROVISIONED
+    table_class  = "STANDARD"        # STANDARD / STANDARD_INFREQUENT_ACCESS
+    billing_mode = "PAY_PER_REQUEST" # PAY_PER_REQUEST / PROVISIONED
+
+    # 아래 capacity_autoscaling_enable은 PROVISIONED 모드일 때만 true/false를 선택하면 됨
+    capacity_autoscaling_enable = true
+    enable_auto_scaling = {
+      read_capacity_min                 = 1
+      read_capacity_max                 = 10
+      read_capacity_utilization_percent = 70
+
+      write_capacity_min                 = 1
+      write_capacity_max                 = 10
+      write_capacity_utilization_percent = 70
+    }
+    disable_auto_scaling = {
+      read_capacity  = 10
+      write_capacity = 10
+    }
+
+    PITR_enable = true
 
     partition_key = "id"
     sort_key      = null
