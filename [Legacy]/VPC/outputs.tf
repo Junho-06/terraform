c797516684cbc2476e313368ff35f4d28810b633
Junho-06 <k34920032@gmail.com>
2025-03-01 15:20:00 +0900

    delete legacy vpc

--- [Legacy]/VPC/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc" {
-  value = <<EOT
-    vpc_id             = ${jsonencode(aws_vpc.main-vpc.id)}
-    vpc_cidr           = ${jsonencode(aws_vpc.main-vpc.cidr_block)}
-    public_subnets     = ${jsonencode([aws_subnet.public-a.id, aws_subnet.public-b.id])}
-    private_subnets    = ${jsonencode([aws_subnet.private-a.id, aws_subnet.private-b.id])}
-    database_subnets   = ${jsonencode([aws_subnet.database-a.id, aws_subnet.database-b.id])}
-  EOT
-}
