6e33a135918bbf3247d81e36d170bb75b3474a3d
Junho-06 <k34920032@gmail.com>
2025-03-07 13:21:03 +0900

    update ec2 output ssh command

--- EC2-bastion/outputs.tf
+++ EC2-bastion/outputs.tf
@@ -7,5 +7,7 @@ output "bastion" {
     Instance_SSH_CIDR = "${var.ec2.ssh-cidr-myip == true ? "${chomp(data.http.myip[0].response_body)}/32" : "0.0.0.0/0"}"
 
     Instance_Attached_Policy_ARN = "arn:aws:iam::aws:policy/${var.ec2.attach-policy == "Admin" ? "AdministratorAccess" : var.ec2.attach-policy == "Poweruser" ? "PowerUserAccess" : null}"
+
+    SSH_Connnect_Command = "ssh -i ${var.key}.pem ${startswith(var.ec2.ec2_ami, "Amazon") ? "ec2-user" : "ubuntu"}@${var.ec2.map_eip_to_bastion == true ? aws_eip.bastion-eip[0].public_ip : aws_instance.bastion.public_ip}"
   }
 }
