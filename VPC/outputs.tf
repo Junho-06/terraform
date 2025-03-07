8605bb10a2a3d663211c658f1e07f4f3146d8231
Junho-06 <k34920032@gmail.com>
2025-03-07 13:11:27 +0900

    update VPC output

--- VPC/outputs.tf
+++ VPC/outputs.tf
@@ -9,7 +9,7 @@ output "VPC" {
 
     Database_Route_Table_Seperate = var.vpc.database.database_route_table_seperate
 
-    VPC_Flowlog_ID = aws_flow_log.vpc-flowlog[0].id
+    VPC_Flowlog_ID = try(aws_flow_log.vpc-flowlog[0].id, null)
 
     Created_Endpoint_List = try(var.vpc.endpoint.service_names, [])
   }
