04af07a0a8fb72297d4ef333783f989f869ce339
Junho-06 <k34920032@gmail.com>
2025-03-13 10:55:34 +0900

    update rds output null

--- RDS-single/outputs.tf
+++ RDS-single/outputs.tf
@@ -5,7 +5,7 @@ output "rds" {
       Reader_Endpoint = var.db_engine.create_aurora_mysql_cluster == true ? aws_rds_cluster.aurora-mysql-cluster[0].reader_endpoint : null
 
       Username = var.db_engine.create_aurora_mysql_cluster == true ? var.aurora-mysql.master_username : null
-      Passwrod = var.db_engine.create_aurora_mysql_cluster == true && var.aurora-mysql.manage_master_user_password == true ? nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.mysql_secrets_current[0].secret_string)["password"]) : try(var.aurora-mysql.master_password, null)
+      Passwrod = var.db_engine.create_aurora_mysql_cluster == true && var.aurora-mysql.manage_master_user_password == true ? nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.mysql_secrets_current[0].secret_string)["password"]) : var.aurora-mysql.manage_master_user_password == false ? try(var.aurora-mysql.master_password, null) : null
 
       Port = var.db_engine.create_aurora_mysql_cluster == true ? aws_rds_cluster.aurora-mysql-cluster[0].port : null
     }
@@ -15,7 +15,7 @@ output "rds" {
       Reader_Endpoint = var.db_engine.create_aurora_postgres_cluster == true ? aws_rds_cluster.aurora-postgres-cluster[0].reader_endpoint : null
 
       Username = var.db_engine.create_aurora_postgres_cluster == true ? var.aurora-postgres.master_username : null
-      Passwrod = var.db_engine.create_aurora_postgres_cluster == true && var.aurora-postgres.manage_master_user_password == true ? nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.postgres_secrets_current[0].secret_string)["password"]) : try(var.aurora-postgres.master_password, null)
+      Passwrod = var.db_engine.create_aurora_postgres_cluster == true && var.aurora-postgres.manage_master_user_password == true ? nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.postgres_secrets_current[0].secret_string)["password"]) : var.aurora-postgres.manage_master_user_password == false ? try(var.aurora-postgres.master_password, null) : null
 
       Port = var.db_engine.create_aurora_postgres_cluster == true ? aws_rds_cluster.aurora-postgres-cluster[0].port : null
     }
